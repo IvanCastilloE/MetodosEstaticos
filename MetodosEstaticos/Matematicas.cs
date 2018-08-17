eb473e009c20224f39c6c239738d030949a06b88
Alumno Ulsa <alumno@ulsa.ulsa-noroeste.edu.mx>
2018-08-17 07:39:28 -0700

    Agregar archivos de proyecto.

---
+++ MetodosEstaticos/Matematicas.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MetodosEstaticos
+{
+    class Matematicas
+    {
+        //2 
+        public static int Potencia(int numeroBase, int exponente)
+        {
+            int resultado = 1;
+            for (int i= 0; i<exponente; i++)
+            {
+                resultado = resultado * numeroBase;
+            }
+            return resultado;
+        }
+    }
+}
---
+++ MetodosEstaticos/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MetodosEstaticos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int dosElevadoATres =
+                Matematicas.Potencia(2, 3);
+            Console.WriteLine("Resultado: " + dosElevadoATres);
+            Console.ReadLine();
+        }
+    }
+}
